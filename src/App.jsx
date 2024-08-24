535b7dfcc731115523c23cb000464d3ce2c5c991
mohammed kaif <mkaif555123@gmail.com>
2024-08-25 00:53:04 +0500

    30% Work Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from "react"
+import { useState } from "react";
 
 function App() {
-
-  const [color , setColor] = useState("olive")
+  const [color, setColor] = useState("olive");
   return (
-    <div className="w-full h-screen" style={{backgroundColor: color}}>
-
+    <div className="w-full h-screen" style={{ backgroundColor: color }}>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">Check</div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
