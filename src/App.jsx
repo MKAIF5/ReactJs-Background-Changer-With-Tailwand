da1bb4ee84291406cc72399153a12f6d3313d399
mohammed kaif <mkaif555123@gmail.com>
2024-08-25 01:22:41 +0500

    40% work completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,13 @@ function App() {
   const [color, setColor] = useState("olive");
   return (
     <div className="w-full h-screen" style={{ backgroundColor: color }}>
-      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">Check</div>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 
+        shadow-lg bg-white px-3 py-2 rounded-3xl">
+        <button className="outline-none px-4 py-1 rounded-full text-white"
+        style={{backgroundColor: "red"}}>Red</button>          
+        </div>
+      </div>
     </div>
   );
 }
